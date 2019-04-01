db6c01a6f45b7434110abdf1c1aedc67778536d8
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-04-01 16:57:01 +0200

    MIME best practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 # Parent class for controllers
 class ApplicationController < ActionController::API
-  before_action :set_headers
-
-  # Set the right content type for json respecting https://jsonapi.org/
-  def set_headers
-    response.headers['Content-Type'] = 'application/vnd.api+json'
-  end
-
   # Checks if the request has an header Authorization with the right token
   def authorize_request
     header = request.headers['Authorization']
