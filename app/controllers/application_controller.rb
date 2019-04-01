107086d61c3de80fe337b870938a281567d51745
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-04-01 15:51:17 +0200

    MIME best practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 # Parent class for controllers
 class ApplicationController < ActionController::API
-  before_action :set_headers
-
-  # Set the right content type for json respecting https://jsonapi.org/
-  def set_headers
-    response.headers['Content-Type'] = 'application/vnd.api+json'
-  end
 end
